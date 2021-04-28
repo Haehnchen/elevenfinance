f802a3b583a7a9bb9c083e7acf53b562aa26b87e
Ursa <77991751+ursa-trades@users.noreply.github.com>
2021-04-28 13:45:15 +0200

    Add farm config to old EPS-BNB LP pool

--- src/features/configure/pools.js
+++ src/features/configure/pools.js
@@ -449,7 +449,15 @@ export const pools = [
     pricePerFullShare: 1,
     pastPricePerFullShare: 1,
     tokenAddress: '0xf9045866e7b372DeF1EFf3712CE55FAc1A98dAF0',
-    categories: ['Ellipsis']
+    categories: ['Ellipsis'],
+
+    farm: {
+      earnedToken: 'ELE',
+      earnedTokenDecimals: 18,
+      earnedTokenAddress: '0xAcD7B3D9c10e97d0efA418903C0c7669E702E4C0',
+      earnContractAddress: '0x1ac6c0b955b6d7acb61c9bdf3ee98e0689e07b8a',
+      masterchefPid: 77
+    }
   },
   {
     id: 'xedbnb',
