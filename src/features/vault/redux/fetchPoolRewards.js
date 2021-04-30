a71f53a6e9e69d1054e206c38ee30078fdbadfc9
Ursa <77991751+ursa-trades@users.noreply.github.com>
2021-04-30 17:54:31 +0200

    Fix the delay before displaying pending rewards

--- src/features/vault/redux/fetchPoolRewards.js
+++ src/features/vault/redux/fetchPoolRewards.js
@@ -153,7 +153,10 @@ export function reducer(state, action) {
 
       return {
         ...state,
-        pendingRewards: pendingRewards,
+        pendingRewards: {
+          ...pendingRewards,
+          [action.id]: action.data
+        },
         fetchPoolRewardsDone,
         fetchPoolRewardsPending,
       };
