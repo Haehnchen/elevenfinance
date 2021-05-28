fa685939967adb94f351b2fdcb3daf7c37940a56
kk <11eleven11finance@gmail.com>
2021-05-28 03:44:07 +0200

    add farms

--- src/features/configure/pools.js
+++ src/features/configure/pools.js
@@ -199,13 +199,13 @@ export const pools = [
     tokenAddress: '0x85dE135fF062Df790A5f20B79120f17D3da63b2d',
     categories: ['Sushi'],
 
-    // farm: {
-    //   earnedToken: 'ELE',
-    //   earnedTokenDecimals: 18,
-    //   earnedTokenAddress: '0xAcD7B3D9c10e97d0efA418903C0c7669E702E4C0',
-    //   earnContractAddress: '0xD109D9d6f258D48899D7D16549B89122B0536729',
-    //   masterchefPid: 2
-    // },
+     farm: {
+       earnedToken: 'ELE',
+       earnedTokenDecimals: 18,
+       earnedTokenAddress: '0xAcD7B3D9c10e97d0efA418903C0c7669E702E4C0',
+       earnContractAddress: '0xD109D9d6f258D48899D7D16549B89122B0536729',
+       masterchefPid: 3
+     },
 
     // TODO: set correct pool fees
     fees: {
@@ -239,13 +239,13 @@ export const pools = [
     tokenAddress: '0xA79983Daf2A92c2C902cD74217Efe3D8AF9Fba2a',
     categories: ['Sushi'],
 
-    // farm: {
-    //   earnedToken: 'ELE',
-    //   earnedTokenDecimals: 18,
-    //   earnedTokenAddress: '0xAcD7B3D9c10e97d0efA418903C0c7669E702E4C0',
-    //   earnContractAddress: '0xD109D9d6f258D48899D7D16549B89122B0536729',
-    //   masterchefPid: 2 // TODO:
-    // },
+     farm: {
+       earnedToken: 'ELE',
+       earnedTokenDecimals: 18,
+       earnedTokenAddress: '0xAcD7B3D9c10e97d0efA418903C0c7669E702E4C0',
+       earnContractAddress: '0xD109D9d6f258D48899D7D16549B89122B0536729',
+       masterchefPid: 4 // TODO:
+     },
 
     // TODO: set correct pool fees
     fees: {
@@ -279,13 +279,13 @@ export const pools = [
     tokenAddress: '0x35c1895DAC1e2432b320e2927b4F71a0D995602F',
     categories: ['Sushi'],
 
-    // farm: {
-    //   earnedToken: 'ELE',
-    //   earnedTokenDecimals: 18,
-    //   earnedTokenAddress: '0xAcD7B3D9c10e97d0efA418903C0c7669E702E4C0',
-    //   earnContractAddress: '0xD109D9d6f258D48899D7D16549B89122B0536729',
-    //   masterchefPid: 2 // TODO:
-    // },
+     farm: {
+       earnedToken: 'ELE',
+       earnedTokenDecimals: 18,
+       earnedTokenAddress: '0xAcD7B3D9c10e97d0efA418903C0c7669E702E4C0',
+       earnContractAddress: '0xD109D9d6f258D48899D7D16549B89122B0536729',
+       masterchefPid: 5 // TODO:
+     },
 
     // TODO: set correct pool fees
     fees: {
@@ -319,13 +319,13 @@ export const pools = [
     tokenAddress: '0xaAa5B9e6c589642f98a1cDA99B9D024B8407285A',
     categories: [],
 
-    // farm: {
-    //   earnedToken: 'ELE',
-    //   earnedTokenDecimals: 18,
-    //   earnedTokenAddress: '0xAcD7B3D9c10e97d0efA418903C0c7669E702E4C0',
-    //   earnContractAddress: '0xD109D9d6f258D48899D7D16549B89122B0536729',
-    //   masterchefPid: 2 // TODO:
-    // },
+     farm: {
+       earnedToken: 'ELE',
+       earnedTokenDecimals: 18,
+       earnedTokenAddress: '0xAcD7B3D9c10e97d0efA418903C0c7669E702E4C0',
+       earnContractAddress: '0xD109D9d6f258D48899D7D16549B89122B0536729',
+       masterchefPid: 6 // TODO:
+     },
 
     // TODO: set correct pool fees
     fees: {
